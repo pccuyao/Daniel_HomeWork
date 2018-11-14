e52292a0ca2b5fef86d4faf04a8088b517f4f1a0
pccuyao <40812714+pccuyao@users.noreply.github.com>
2018-11-15 03:22:40 +0800

    add file

---
+++ 20181115(2)/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    string str;
+    for(;;) //star to scan
+    {
+        if(str.size()<10) // if string size
+        {
+            ch=getch(); // get char
+            if(ch == 8) // if char is (Backspace, ASII 8)
+                {
+                    str.erase(str.end() -1); // erase it
+                }
+                    else
+                {
+                        str = str + ch; // add it
+                }
+            cout << "<" << str.size() << ">" << str << endl; // print it
+        }
+            else
+        {
+            cout << endl << "over!" << endl; //end it
+            return 0;
+        }
+    }
+
+    return 0;
+}
